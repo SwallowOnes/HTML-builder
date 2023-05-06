ba3d883cd31eda994eab689c221e5d8827173fc8
SwallowOnes <jager.selion@gmail.com>
2023-05-06 23:29:32 +0500

    fix: solve task 02

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const {stdin, stdout, exit} = require('process')
+
+function closeFILE() {
+    console.log('File closed');
+    writeTxtStream.end();
+    process.exit();
+}
+
+let textFileName = 'text.txt'
+const pathTxtFile = path.join(__dirname, textFileName);
+const writeTxtStream = fs.createWriteStream(pathTxtFile);
+
+stdout.write('File opened. Input text:\n')
+
+stdin.on('data', (text) => {
+    text = text.toString();
+    text.trim() === 'exit' ? closeFILE() : writeTxtStream.write(text);
+});
+
+process.on('SIGINT', ()=> closeFILE());
