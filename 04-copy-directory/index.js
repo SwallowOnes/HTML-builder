3bcec83820255534a0952afd47ab6a7e7a6f60b8
SwallowOnes <jager.selion@gmail.com>
2023-05-08 00:15:36 +0500

    fix: solve task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 
+const folderName = 'files';
+const pathToFolder = path.join(__dirname, folderName);
+const copyFolderName = 'files-copy';
+const copyPathToFolder = path.join(__dirname, copyFolderName);
+
+fs.mkdir(copyPathToFolder, {recursive: true}, (err) => {
+    if(err) throw err;
+    fs.readdir(pathToFolder, (err, files) => {
+        if(err) throw err;
+        files.forEach((file) => {
+            let filePath = path.join(pathToFolder, file);
+            let copyFilePath = path.join(copyPathToFolder, file);
+            fs.copyFile(filePath, copyFilePath, err => {
+                if(err) throw err;
+            });
+        });
+    });
+});
