19fbe3917e4a9824051c3f2ba779c9aa317531d3
SwallowOnes <jager.selion@gmail.com>
2023-05-09 01:50:28 +0500

    feat: add assets task 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,36 +5,74 @@ let cssStyles = [];
 
 const pathToProj = path.join(__dirname, 'project-dist');
 const pathToStyles = path.join(__dirname, 'styles');
+const pathToProjAssets = path.join(pathToProj, 'assets');
+const pathToAssets = path.join(__dirname, 'assets');
 
 
 //css
 fs.readdir(pathToStyles, async (err, files) => {
-    if (err) throw err;
-    const promises = [];
-    files.forEach((file) => {
-        const promise = new Promise((resolve, reject) => {
-            console.log(file);
-            if (file.split('.').at(-1) === 'css') {
-                const pathToFile = path.join(pathToStyles, file);
-                const readTxtStream = fs.createReadStream(pathToFile, 'utf-8');
-                readTxtStream.on('data', (chunk) => {
-                    resolve(chunk);
-                })
-            } else {
-                resolve(null);
-            }
+  if (err) throw err;
+  const promises = [];
+  files.forEach((file) => {
+    const promise = new Promise((resolve, reject) => {
+      // console.log(file);
+      if (file.split('.').at(-1) === 'css') {
+        const pathToFile = path.join(pathToStyles, file);
+        const readTxtStream = fs.createReadStream(pathToFile, 'utf-8');
+        readTxtStream.on('data', (chunk) => {
+          resolve(chunk);
         })
-        promises.push(promise);
+      } else {
+        resolve(null);
+      }
     })
-    const result = await Promise.all(promises);
-    cssStyles = result
-        .filter(Boolean)
-        .join('')
-    // const pathToProj = 'project-dist';
-    const pathToCssStyle = path.join(__dirname, 'project-dist', 'style.css');
-    fs.writeFile(pathToCssStyle, cssStyles, err => {
-        if (err) {
-            throw err
-        };
-    });
+    promises.push(promise);
+  })
+  const result = await Promise.all(promises);
+  cssStyles = result
+    .filter(Boolean)
+    .join('')
+  // const pathToProj = 'project-dist';
+  const pathToCssStyle = path.join(__dirname, 'project-dist', 'style.css');
+  fs.writeFile(pathToCssStyle, cssStyles, err => {
+    if (err) {
+      throw err
+    };
+  });
 });
+
+//assets-copy
+async function copyAssets(pathToAssets, pathToProj) {
+  try {
+    await fs.mkdir(pathToProj, { recursive: true }, (err) => {
+      if (err) throw err;
+      fs.readdir(pathToAssets, (err, files) => {
+        if (err) throw err;
+        files.forEach((file) => {
+          let filePath = path.join(pathToAssets, file);
+          let copyFilePath = path.join(pathToProj, file);
+          fs.stat(filePath, (err, stats) => {
+            if (err) console.log(err);
+            if (stats.isFile()) {
+              fs.copyFile(filePath, copyFilePath, err => {
+                if (err) {
+                  throw err
+                };
+              });
+            } else {
+              copyAssets(filePath, copyFilePath);
+            }
+          });
+        });
+      });
+    });
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+copyAssets(pathToAssets, pathToProjAssets);
+
+
+
