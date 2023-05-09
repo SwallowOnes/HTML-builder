0e4d7a604764efbe0aed30782435145eb5a63531
SwallowOnes <jager.selion@gmail.com>
2023-05-09 21:45:16 +0500

    fix: fix 04 rewrite files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,8 +6,25 @@ const pathToFolder = path.join(__dirname, folderName);
 const copyFolderName = 'files-copy';
 const copyPathToFolder = path.join(__dirname, copyFolderName);
 
+// fs.rmdir(copyPathToFolder, err => {
+//     if(err) {
+//         throw err;
+//      } // не удалось удалить папку
+//      fs.unlink('folder/file_delete.txt', err => {
+//         if(err) throw err; // не удалось удалить файл
+//         console.log('Файл успешно удалён');
+//      });
+//  });
 fs.mkdir(copyPathToFolder, {recursive: true}, (err) => {
     if(err) throw err;
+    fs.readdir(copyPathToFolder, (err, files) => {
+        if(err) throw err;
+        files.forEach((file) => {
+        fs.unlink(path.join(copyPathToFolder, file), (err) => {
+          if (err) throw err;
+        });
+      });
+    });
     fs.readdir(pathToFolder, (err, files) => {
         if(err) throw err;
         files.forEach((file) => {
